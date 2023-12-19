d1426fcd3a72c33f9c818379d86a7d4b373659ad
tiomashimon <tiomashimon@gmai.com>
2023-12-19 20:00:24 +0100

    Models

--- core_apps/post/models.py
+++ core_apps/post/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ..user.models import UserProfile
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+
+
+class DailyLikes(models.Model):
+    date = models.DateField(unique=True)
+    likes_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.date} - {self.likes_count} likes"
+    
+
+
